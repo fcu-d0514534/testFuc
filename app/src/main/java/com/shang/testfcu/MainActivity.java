bbc81f29fad5c56e1b1aa18e05c280d4efb06d69
fuc-d0514534 <D0514534@gmail.com>
2017-04-17 21:43:34 +0800

    2017/4/17 GoogleMap. setOnMyLocationLister

--- app/src/main/java/com/shang/testfcu/MainActivity.java
+++ app/src/main/java/com/shang/testfcu/MainActivity.java
@@ -3,6 +3,7 @@ package com.shang.testfcu;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextClock;
@@ -21,6 +22,8 @@ public class MainActivity extends AppCompatActivity implements FunctionListener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+;
 
         init();
         bt1.setOnClickListener(new View.OnClickListener() {
