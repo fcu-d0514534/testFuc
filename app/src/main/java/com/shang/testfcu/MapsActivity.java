5cd0cfec53928f0175cb38fefca71af4f9c8c2c9
fuc-d0514534 <D0514534@gmail.com>
2017-04-17 20:09:54 +0800

    2017/4/17 GoogleMap. setOnMyLocationLister

--- app/src/main/java/com/shang/testfcu/MapsActivity.java
+++ app/src/main/java/com/shang/testfcu/MapsActivity.java
@@ -1,6 +1,12 @@
 package com.shang.testfcu;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +15,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -19,10 +28,15 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
+        ,GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
     private ArrayList<YouBike> youBikes;
+
+    private Location mLocation;
+    private GoogleApiClient mGoogleApiClient;
+    protected static final String TAG="MapsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +47,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        youBikes=new ArrayList<>();
-        youBikes= (ArrayList<YouBike>) getIntent().getSerializableExtra("youbike");
-
+        youBikes = new ArrayList<>();
+        youBikes = (ArrayList<YouBike>) getIntent().getSerializableExtra("youbike");
+        buildGoogleApiClient();
     }
 
     @Override
@@ -50,9 +64,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").snippet("TEST"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-        Marker marker=mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").snippet("test"));
+        Marker marker = mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").snippet("test"));
         mMap.setInfoWindowAdapter(adapter);
 
+        mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                Toast.makeText(MapsActivity.this,"TEST mMAp",Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });;
+        mMap.setMyLocationEnabled(true);
 
 
     }
@@ -72,10 +94,54 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             tv1.setText(marker.getTitle());
             tv2.setText(marker.getSnippet());
             ig.setImageResource(R.drawable.appicon);
-
             return viwe;
         }
     };
+
+    protected synchronized void buildGoogleApiClient(){
+        mGoogleApiClient=new GoogleApiClient.Builder(MapsActivity.this)
+                .addConnectionCallbacks(MapsActivity.this)
+                .addOnConnectionFailedListener(MapsActivity.this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocation=LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(mLocation!=null){
+            Log.d(TAG,mLocation.getLatitude()+"");
+            Log.d(TAG,mLocation.getLongitude()+"");
+        }else{
+            Toast.makeText(this,"NULL",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + connectionResult.getErrorCode());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
 
 
 }
